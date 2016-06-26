14b615123b5e89510a91ca877bc02f9350db59ac
peromax <piarmx@gmail.com>
2016-06-26 15:46:47 +0300

    added support current_shard for delay paperclip
    added support current_shard for delay paperclip

--- lib/delayed_paperclip/jobs/sidekiq.rb
+++ lib/delayed_paperclip/jobs/sidekiq.rb
@@ -1,29 +1,17 @@
 require 'sidekiq/worker'
-require 'active_support/deprecation'
 
 module DelayedPaperclip
   module Jobs
     class Sidekiq
       include ::Sidekiq::Worker
+      sidekiq_options :queue => :paperclip
 
-      def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
-        ActiveSupport::Deprecation.warn(<<-MESSAGE)
-Using Sidekiq adapter for delayed_paperclip is deprecated and will be removed in version 3.0.0.
-Please use ActiveJob adapter.
-        MESSAGE
-
-        queue_name = instance_klass.constantize.paperclip_definitions[attachment_name][:delayed][:queue]
-        # Sidekiq >= 4.1.0
-        if respond_to?(:set)
-          set(:queue => queue_name)
-        else
-          sidekiq_options :queue => queue_name
-        end
-        perform_async(instance_klass, instance_id, attachment_name)
+      def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name, shard)
+        perform_async(instance_klass, instance_id, attachment_name, shard)
       end
 
-      def perform(instance_klass, instance_id, attachment_name)
-        DelayedPaperclip.process_job(instance_klass, instance_id, attachment_name)
+      def perform(instance_klass, instance_id, attachment_name, shard)
+        DelayedPaperclip.process_job(instance_klass, instance_id, attachment_name, shard)
       end
     end
   end
